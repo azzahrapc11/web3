cc9cdc77e9a37bc7ac328e9e01a3132080c55df6
azzahrapc11 <azzahrapc11>
2019-05-23 09:34:08 +0700

    Updates

---
+++ precache-manifest.3358d72cd8a387d010fe633af7faea50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57095bf24f2d6481d7f2ac59ec1de5c6",
+    "url": "/web3/index.html"
+  },
+  {
+    "revision": "efa5f5a86552d6b700da",
+    "url": "/web3/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/web3/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "efa5f5a86552d6b700da",
+    "url": "/web3/static/js/main.8b8d45ff.chunk.js"
+  },
+  {
+    "revision": "41c932d437fb8ea8fe33",
+    "url": "/web3/static/js/runtime~main.e271968f.js"
+  }
+]);
